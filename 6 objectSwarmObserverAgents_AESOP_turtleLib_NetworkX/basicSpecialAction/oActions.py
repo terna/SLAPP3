92afcda0c468d7b6737649e2ccb9a0bd3fc7836d
Pietro Terna <pietro.terna@unito.it>
2017-06-03 12:39:30 +0200

    applying PEP8 to basicSpecialAction

--- 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/basicSpecialAction/oActions.py
+++ 6 objectSwarmObserverAgents_AESOP_turtleLib_NetworkX/basicSpecialAction/oActions.py
@@ -1,29 +1,34 @@
 from Tools import *
 from Agent import *
 
+
 def do1b(address):
     pass
 
-def do2a(address,cycle):
-            self=address # if necessary
 
-            # ask each agent, without parameters
+def do2a(address, cycle):
+    self = address  # if necessary
 
-            print("Time = ", cycle, "ask all agents to report position")
-            askEachAgentInCollection(address.modelSwarm.getAgentList(),Agent.reportPosition)
+    # ask each agent, without parameters
+
+    print("Time = ", cycle, "ask all agents to report position")
+    askEachAgentInCollection(
+        address.modelSwarm.getAgentList(),
+        Agent.reportPosition)
 
 
-def do2b(address,cycle):
-            self=address # if necessary
+def do2b(address, cycle):
+    self = address  # if necessary
 
-            # ask a single agent, without parameters
-            print("Time = ",cycle,"ask first agent to report position")
-            if address.modelSwarm.getAgentList() != []:
-                askAgent(address.modelSwarm.getAgentList()[0],\
-                         Agent.reportPosition)
+    # ask a single agent, without parameters
+    print("Time = ", cycle, "ask first agent to report position")
+    if address.modelSwarm.getAgentList() != []:
+        askAgent(address.modelSwarm.getAgentList()[0],
+                 Agent.reportPosition)
+
 
 def otherSubSteps(subStep, address):
-            return False
+    return False
 
 
 # an example of special action code, to be activated if the time
